4657f1b348287d7c10a7e4b707cf1fdfd58754a8
om <dodiyanihar8460@gmail.com>
2022-10-11 12:56:40 +0530

    Update forgot_password_controller.dart

--- lib/screen/manager_section/auth_manager/forgot_Password/forgot_password_controller.dart
+++ lib/screen/manager_section/auth_manager/forgot_Password/forgot_password_controller.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
@@ -13,43 +14,11 @@ class ForgotPasswordControllerM extends GetxController {
 
   static FirebaseFirestore fireStore = FirebaseFirestore.instance;
 
-
-  // var acs = ActionCodeSettings(
-  //     // URL you want to redirect back to. The domain (www.example.com) for this
-  //     // URL must be whitelisted in the Firebase Console.
-  //
-  //     url: 'https://jobseek.page.link/6SuK',
-  //     // This must be true
-  //     handleCodeInApp: true,
-  //     iOSBundleId: 'com.example.ios',
-  //     androidPackageName: 'com.example.android',
-  //     // installIfNotAvailable
-  //     androidInstallApp: true,
-  //     // minimumVersion
-  //     androidMinimumVersion: '12');
-
-  // FirebaseAuth.instance.sendSignInLinkToEmail(
-  // email: emailAuth, actionCodeSettings: acs)
-  //     .catchError((onError) => print('Error sending email verification $onError'))
-  //     .then((value) => print('Successfully sent email verification'));
-
-  // Future verificationEmail(email) async {
-  //   try {
-  //     FirebaseAuth.instance
-  //         .sendSignInLinkToEmail(email: email, actionCodeSettings: acs);
-  //   } catch (err) {
-  //     if (kDebugMode) {
-  //       print(err);
-  //     }
-  //   }
-  // }
-
   emailValidation() {
     if (forgotEmailController.text.trim() == "") {
       emailError = 'Please Enter email';
     } else {
-      if (RegExp(
-              r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+      if (RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
           .hasMatch(forgotEmailController.text)) {
         // return 'Enter  valid Email';
         emailError = '';
@@ -61,7 +30,6 @@ class ForgotPasswordControllerM extends GetxController {
 
   bool validator() {
     emailValidation();
-
     if (emailError == "") {
       return true;
     } else {
