0201fa8cc02601c63ae77f8e81c69b7f0d918980
mobileappdev47 <mobileappdev47@gmail.com>
2022-11-07 09:37:01 +0530

    all job changes

--- lib/screen/dashboard/home/widgets/all_jobs.dart
+++ lib/screen/dashboard/home/widgets/all_jobs.dart
@@ -9,157 +9,144 @@ import 'package:jobseek/utils/app_style.dart';
 import 'package:jobseek/utils/asset_res.dart';
 import 'package:jobseek/utils/color_res.dart';
 
-Widget allJobs(Stream stream){
+Widget allJobs(Stream stream) {
   final HomeController controller = HomeController();
 
   return GetBuilder<JobRecommendationController>(
       id: "search",
-      builder: (jrController){
+      builder: (jrController) {
         return StreamBuilder(
             stream: stream,
             builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
-
-              if (snapshot.connectionState ==
-                  ConnectionState.waiting) {
-                return const Center(
-                    child: CircularProgressIndicator(
-
-                    ));
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Center(child: CircularProgressIndicator());
               }
 
               jrController.documents = snapshot.data.docs;
 
               if (jrController.searchText.value.isNotEmpty) {
-                jrController.documents = jrController.documents.where((element) {
-
-
+                jrController.documents =
+                    jrController.documents.where((element) {
                   return element
                       .get('Position')
                       .toString()
                       .toLowerCase()
                       .contains(jrController.searchText.value.toLowerCase());
-
                 }).toList();
-
               }
-
 
               return snapshot.hasData
                   ? ListView.builder(
-                  itemCount: jrController.documents.length,
-                  shrinkWrap: true,
-                  physics: const BouncingScrollPhysics(),
-                  itemBuilder: (context, index) {
-                    if (kDebugMode) {
-                      print(index);
-                    }
-                    if (kDebugMode) {
-                      print(jrController.documents[index].id);
-                    }
-                    return InkWell(
-                      onTap: () => Get.toNamed(
-                          AppRes.jobDetailScreen,
-                          arguments: {
-                            "saved": controller
-                                .jobTypesSaved[index % 5],
+                      itemCount: jrController.documents.length,
+                      shrinkWrap: true,
+                      physics: const BouncingScrollPhysics(),
+                      itemBuilder: (context, index) {
+                        if (kDebugMode) {
+                          print(index);
+                        }
+                        if (kDebugMode) {
+                          print(jrController.documents[index].id);
+                        }
+                        return InkWell(
+                          onTap: () =>
+                              Get.toNamed(AppRes.jobDetailScreen, arguments: {
+                            "saved": controller.jobTypesSaved[index % 5],
                             "docId": snapshot.data!.docs[index].id,
-                            "doc":snapshot.data!.docs[index],
+                            "doc": snapshot.data!.docs[index],
                           }),
-
-                      child: Container(
-                        height: 92,
-                        width: Get.width,
-                        margin: const EdgeInsets.symmetric(
-                            horizontal: 18, vertical: 4),
-                        padding: const EdgeInsets.all(15),
-                        decoration: BoxDecoration(
-                            borderRadius: const BorderRadius.all(
-                                Radius.circular(15)),
-                            border: Border.all(
-                                color: const Color(0xffF3ECFF)),
-                            color: ColorRes.white),
-                        child: Row(
-                          children: [
-                            Image.asset(controller
-                                .jobTypesLogo[index % 5]),
-                            const SizedBox(width: 20),
-                            Column(
-                              mainAxisAlignment:
-                              MainAxisAlignment.center,
-                              crossAxisAlignment:
-                              CrossAxisAlignment.start,
+                          child: Container(
+                            height: 92,
+                            width: Get.width,
+                            margin: const EdgeInsets.symmetric(
+                                horizontal: 18, vertical: 4),
+                            padding: const EdgeInsets.all(15),
+                            decoration: BoxDecoration(
+                                borderRadius:
+                                    const BorderRadius.all(Radius.circular(15)),
+                                border:
+                                    Border.all(color: const Color(0xffF3ECFF)),
+                                color: ColorRes.white),
+                            child: Row(
                               children: [
-                                Text(
-                                  // controller.jobTypes[index],
-                                    jrController.documents[index]
-                                    ["Position"],
-                                    style: appTextStyle(
-                                        color: ColorRes.black,
-                                        fontSize: 15,
-                                        fontWeight:
-                                        FontWeight.w500)),
-                                Text(
-                                    jrController.documents[index]
-                                    ["CompanyName"],
-                                    style: appTextStyle(
-                                        color: ColorRes.black,
-                                        fontSize: 12,
-                                        fontWeight:
-                                        FontWeight.w400)),
-                                Text(
-                                    "${jrController.documents[index]["location"]}  ${jrController.documents[index]["type"]}",
-                                    style: appTextStyle(
-                                        color: ColorRes.black,
-                                        fontSize: 10,
-                                        fontWeight:
-                                        FontWeight.w400)),
-                              ],
-                            ),
-                            const Spacer(),
-                            Column(
-                              mainAxisAlignment:
-                              MainAxisAlignment.start,
-                              crossAxisAlignment:
-                              CrossAxisAlignment.end,
-                              children: [
-                                InkWell(
-                                  onTap: () {
-                                    controller.onTapSave(index);
-                                  },
-                                  child: Obx(() {
-                                    return Image.asset(
-                                      controller.jobTypesSaved[
-                                      index % 5]
-                                          ? AssetRes
-                                          .bookMarkFillIcon
-                                          : AssetRes
-                                          .bookMarkBorderIcon,
-                                      height: 20,
-                                    );
-                                  }),
+                                Image.asset(controller.jobTypesLogo[index % 5]),
+                                const SizedBox(width: 20),
+                                Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                        jrController.documents[index]
+                                            ["Position"],
+                                        style: appTextStyle(
+                                            color: ColorRes.black,
+                                            fontSize: 15,
+                                            fontWeight: FontWeight.w500)),
+                                    Text(
+                                        jrController.documents[index]
+                                            ["CompanyName"],
+                                        style: appTextStyle(
+                                            color: ColorRes.black,
+                                            fontSize: 12,
+                                            fontWeight: FontWeight.w400)),
+                                    Text(
+                                        "${jrController.documents[index]["location"]}  ${jrController.documents[index]["type"]}",
+                                        style: appTextStyle(
+                                            color: ColorRes.black,
+                                            fontSize: 10,
+                                            fontWeight: FontWeight.w400)),
+                                  ],
                                 ),
                                 const Spacer(),
-                                Text(
-                                  "\$${jrController.documents[index]["salary"]}",
-                                  style: appTextStyle(
-                                      fontSize: 16,
-                                      color:
-                                      ColorRes.containerColor,
-                                      fontWeight:
-                                      FontWeight.w500),
-                                )
+                                Column(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  crossAxisAlignment: CrossAxisAlignment.end,
+                                  children: [
+                                    InkWell(
+                                      onTap: () {
+                                        controller.onTapSave(index);
+                                      },
+                                      child: Obx(() {
+                                        return Image.asset(
+                                          controller.jobTypesSaved[index % 5]
+                                              ? AssetRes.bookMarkFillIcon
+                                              : AssetRes.bookMarkBorderIcon,
+                                          height: 20,
+                                        );
+                                      }),
+                                    ),
+                                    const Spacer(),
+                                    Text(
+                                      "\$${jrController.documents[index]["salary"]}",
+                                      style: appTextStyle(
+                                          fontSize: 16,
+                                          color: ColorRes.containerColor,
+                                          fontWeight: FontWeight.w500),
+                                    )
+                                  ],
+                                ),
+                                const SizedBox(width: 10)
                               ],
                             ),
-                            const SizedBox(width: 10)
-                          ],
+                          ),
+                        );
+                      })
+                  : Container(
+                      height: Get.height,
+                      width: Get.width,
+                      color: ColorRes.containerColor.withOpacity(0.2),
+                      alignment: Alignment.center,
+                      child: Container(
+                        padding: const EdgeInsets.all(35),
+                        height: 110,
+                        width: 110,
+                        decoration: BoxDecoration(
+                            color: ColorRes.white,
+                            borderRadius: BorderRadius.circular(25)),
+                        child: const CircularProgressIndicator(
+                          color: ColorRes.containerColor,
                         ),
                       ),
                     );
-                  })
-                  : const Center(
-                child: CircularProgressIndicator(),
-              );
             });
       });
 }
-
